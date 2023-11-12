f695a074e39cca19271bd843de7a19c6b3662b1c
Tomohiro Manabe <tmanabe@users.noreply.github.com>
2023-11-12 22:49:16 +0900

    update Solr

--- solr.sh
+++ solr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t solr:8.11.1 ./solr
-docker run -d --name solr -p 5005:5005 -p 8983:8983 solr:8.11.1
+docker build -t solr:9.2.1 ./solr
+docker run -d --name solr -p 5005:5005 -p 8983:8983 solr:9.2.1
